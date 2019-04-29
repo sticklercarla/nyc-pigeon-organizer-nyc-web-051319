14d672c0134efdedef650b8f0cdc5849c5735596
Carla Stickler <carlastickler@gmail.com>
2019-04-29 18:57:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, attribute|
+    attribute.each do |info, bird_name|
+      bird_name.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name].has_key?(category)
+          pigeon_list[name][category] = []
+        end
+        if !pigeon_list[name][category].include?(info)
+         pigeon_list[name][category] << info.to_s 
+        end
+      end
+    end
+  end
+  pigeon_list
 end
